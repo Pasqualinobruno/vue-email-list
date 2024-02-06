01a6a92e36dc04aa66e502545e6f0d67e09fa6e6
Pasqualibruno <pasqualinomolosso@gmail.com>
2024-02-07 00:46:15 +0100

    Create 10 email

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      emails: [],
+    }
+  },
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+          //console.log(response);
+          this.emails.push(response.data.response)
+          //console.log(this.email);
+          console.log(this.emails);
+        })
+    }
+
+  }
+}
+</script>
+
+<template>
+  <div v-for="email in emails">
+    <p v-if="emails.length <= 10">{{ email }}</p>
+  </div>
+</template>
+
+<style scoped></style>
